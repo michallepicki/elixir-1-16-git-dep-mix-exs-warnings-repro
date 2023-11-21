43b72f978f4a3be51ac00d8b0b779f39deb05948
Michał Łępicki <michallepicki@users.noreply.github.com>
2023-11-21 23:12:46 +0100

    first commit

---
+++ lib/hmmm.ex
@@ -0,0 +1,18 @@
+defmodule Hmmm do
+  @moduledoc """
+  Documentation for `Hmmm`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Hmmm.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/hmmm_test.exs
@@ -0,0 +1,8 @@
+defmodule HmmmTest do
+  use ExUnit.Case
+  doctest Hmmm
+
+  test "greets the world" do
+    assert Hmmm.hello() == :world
+  end
+end
